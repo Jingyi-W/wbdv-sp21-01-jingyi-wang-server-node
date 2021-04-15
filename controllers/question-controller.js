f671645461a352c15acf6224fbf13ed81a6a5c10
jingyiwang <jingyiwang@jingyis-mbp.myfiosgateway.com>
2021-04-15 13:55:10 -0400

    adding hello

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -12,8 +12,14 @@ module.exports = (app) => {
     questionsService.findQuestionsForQuiz(req.params['quizId']).then(questions => res.json(questions))
   }
 
+  const sayHello = (req, res) => {
+    res.send("Hello")
+  }
+
   app.get("/api/questions", findAllQuestions)
   app.get("/api/questions/:questionId", findQuestionById)
   app.get("/api/quizzes/:quizId/questions", findQuestionsForQuiz)
+  
+  app.get("/hello", sayHello)
 }
 
